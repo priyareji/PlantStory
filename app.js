6d357ce96b79a9bacf59cdc8de2f0965d0a5a8b9
Priya Reji <priyareji25@gmail.com>
2023-08-04 10:52:00 +0530

    port changed

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-app.listen(4000, function () {
+app.listen(3000, function () {
   console.log("server is running...");
 });
 
